159c1f44f18e1385dcefa15e6dc7a3fcbfa823c0
thanadon145 <thanadonleelanon@gmail.com>
2025-03-03 21:46:46 +0700

    แก้ menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 document.getElementById("goToPage").addEventListener("click", function() {
     window.location.href = "member.html";
 });
+
+const btn = document.querySelector('.btn');
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', function() {
+    menu.classList.toggle('active');
+});
