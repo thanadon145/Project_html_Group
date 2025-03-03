e0fd291fb32dcc7a0ee88ae150158bd8e30efaa6
thanadon145 <thanadonleelanon@gmail.com>
2025-03-03 21:51:22 +0700

    .

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@ document.getElementById("goToPage").addEventListener("click", function() {
     window.location.href = "member.html";
 });
 
-const btn = document.querySelector('.btn');
-const menu = document.getElementById('menu');
-
-btn.addEventListener('click', function() {
-    menu.classList.toggle('active');
+document.querySelector('.btn').addEventListener('click', function() {
+    alert('คุณคลิกปุ่ม MENU!');
 });
