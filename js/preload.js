48cb2f2222f4ff0fcb60eaa33d0df0a05c8c38b3
thanadon145 <thanadonleelanon@gmail.com>
2025-03-03 21:30:31 +0700

    แก้หน้า load นิดหน่อยยยย

--- js/preload.js
+++ js/preload.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded",function(){
-    let preloader = document.createElement("div")
+document.addEventListener("DOMContentLoaded", function() {
+    let preloader = document.createElement("div");
     preloader.id = "preloader";
-    preloader.innerHTML = '<div class="loader></div>';
-    document.body.prepend(preloader)
+    preloader.innerHTML = '<div class="loader"></div>';
+    document.body.prepend(preloader);
 
-    window.onload = function(){
+    window.onload = function() {
         document.getElementById("preloader").style.display = "none";
         document.getElementById("content").style.display = "block";
     };
