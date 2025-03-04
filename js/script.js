ca7981b45608e51876770ed198f253d1f69730f7
thanadon145 <thanadonleelanon@gmail.com>
2025-03-04 12:24:45 +0700

    ทำ อนิเมชั่นหน้า member

--- js/script.js
+++ js/script.js
@@ -19,3 +19,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setInterval(createShootingStar, 1000);
 });
+
+
+document.querySelector('.member-card').addEventListener('mouseover', function() {
+    this.querySelector('.member-image').src = '2.png';
+});
+
+document.querySelector('.member-card').addEventListener('mouseout', function() {
+    this.querySelector('.member-image').src = '1.png';
+});
+
+function showInfo(cardNumber) {
+    const cards = document.querySelectorAll('.member-card');
+    cards.forEach((card, index) => {
+        if (index + 1 === cardNumber) {
+            card.style.width = '60%';
+            card.style.opacity = '1';
+            card.querySelector('.member-info').style.display = 'block';
+        } else {
+            card.style.width = '20%';
+            card.style.opacity = '0.5';
+            card.querySelector('.member-info').style.display = 'none';
+        }
+    });
+}
